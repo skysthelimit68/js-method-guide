ea5a8a96ac61c6a2f4000f208a16d14c5640938e
Ming Liu <skysthelimit68@gmail.com>
2019-11-06 20:42:33 -0500

    change z-index for active method card, modify hide style

--- js-method-guide/src/Components/MethodCard/index.js
+++ js-method-guide/src/Components/MethodCard/index.js
@@ -23,13 +23,11 @@ class MethodCard extends React.Component {
     }
 
     render() {
-     
-        let method_class = this.state.active ? "method-card-active" : "method-card";
-        let close_class = this.state.active? "close-button" : "hide-close-button";
+
         return(
-            <div className={method_class} key={this.props.method.id} onClick={this.active_click}>
-                <div className="card-icon">{this.props.method.dataType.slice(0,1).toUpperCase()}</div>
-                <div className={close_class} onClick={this.close_button}>X</div>
+            <div className={this.state.active ? "method-card-active" : "method-card"} key={this.props.method.id} onClick={this.active_click}>
+                <div className={this.state.active ? "hide" : "card-icon"}>{this.props.method.dataType.slice(0,1).toUpperCase()}</div>
+                <div className={this.state.active? "close-button" : "hide"} onClick={this.close_button}>X</div>
                 <h2>{this.props.method.name}</h2>
                 <div className="divider"></div>
                 <p>{this.props.method.description}</p>
